fc1c69de89901828391683399babac7d91b89281
Christian <chris.arevir@gmail.com>
2019-12-11 10:27:45 -0800

    [chore]: adding styled components and converting pointer

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
+// import "./styles/index.css";
+import "./styles/reset.css";
+import "./styles/base.css";
+
 import App from "./pages/App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <p className="nes-balloon from-left nes-pointer">
+    This is not a clickable element, but it's an area of the pointer.
+  </p>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
