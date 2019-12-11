3265fc157f2c8811f2bc970be6fd8b6b92caf927
Christian <chris.arevir@gmail.com>
2019-12-11 13:20:34 -0800

    [feature]: moving to .ts files, adding in Text component

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./styles/index.css";
 import "./styles/reset.css";
 import "./styles/base.css";
 
-// import App from "./pages/App";
-import Table from "./components/Table";
+import Text from "./components/Text";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <Table bordered dark centered>
-    <thead>
-      <tr>
-        <th>Table.is-bordered</th>
-        <th>Table.is-centered</th>
-        <th>Table.is-centered</th>
-        <th>Table.is-centered</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Thou hast had a good morning</td>
-        <td>Thou hast had a good afternoon</td>
-        <td>Thou hast had a good evening</td>
-        <td>Thou hast had a good night</td>
-      </tr>
-      <tr>
-        <td>Thou hast had a good morning :D</td>
-        <td>Thou hast had a good afternoon</td>
-        <td>Thou hast had a good evening</td>
-        <td>Thou hast had a good night</td>
-      </tr>
-    </tbody>
-  </Table>,
-  document.getElementById("root")
+const ToView = () => (
+  <>
+    <Text variant="primary">primary</Text>
+    <Text variant="success">success</Text>
+    <Text variant="warning">warning</Text>
+    <Text variant="error">error</Text>
+    <Text variant="disabled">disabled</Text>
+  </>
 );
+
+ReactDOM.render(<ToView />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
