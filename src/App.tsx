3ec145610324c2883bf1ce8920d4c3ec20d6110e
Christian <chris.arevir@gmail.com>
2019-12-12 15:26:15 -0800

    [chore]: copying in description text

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Layout, Menu } from "antd";
-import Icon from "./components/Icon";
 import { HashRouter as Router, Link, Route } from "react-router-dom";
 
 import "./styles/reset.css";
