3419ceeb33c97562b225c9d7984184e99dbb5bc4
Amanda Bosson <amandabosson@yahoo.com>
2019-12-12 13:40:25 -0800

    Add monthly total score to Board view

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -16,6 +16,7 @@ import getClueAndResponse from "../utils/getClueAndResponse";
 import Input from "../components/Input";
 import Table from "../components/Table";
 import Text from "../components/Text";
+import Icon from "../components/Icon";
 
 export const getDayOfWeekLabel = (day: number) => {
   return format(addDays(startOfWeek(new Date()), day), "EEEEEE");
@@ -210,6 +211,9 @@ const Calendar: React.FC<CalendarProps> = ({
           record={record}
         />
       </Table>
+      <div style={{ paddingTop: "1rem" }}>
+        Monthly total: <Icon icon="coin" /> {record.totalScore}
+      </div>
     </>
   );
 };
