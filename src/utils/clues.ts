2ed5ea36a3e92b13721ac7de29cd774ec8dc3ba5
Amanda Bosson <amandabosson@yahoo.com>
2019-12-12 09:38:37 -0800

    Adds clue objects and responses for Dec 1 - 14

--- src/utils/clues.ts
+++ src/utils/clues.ts
@@ -1,6 +1,87 @@
 const clues = {
-  "2019-12-11":
-    "This condiment was touted as an instrument by one starfish in Spongebob Squarepants"
+  "2019-12-01": {
+    category: "IN FULL",
+    clue:
+      'The full Latin name for this 1215 document translates to "the great charter of freedoms"',
+    value: 100
+  },
+  "2019-12-02": {
+    category: "IN FULL",
+    clue:
+      'Despite being part of the name, "and explosives" did not make it into the initials for this government bureau',
+    value: 100
+  },
+  "2019-12-03": {
+    category: "IN FULL",
+    clue:
+      '"The Military College of South Carolina" is in the full name of this school',
+    value: 100
+  },
+  "2019-12-04": {
+    category: "TV MOMS",
+    clue:
+      "During the run of this comedy, actress Debra Jo Rupp & her character Kitty Forman both quit smoking",
+    value: 100
+  },
+  "2019-12-05": {
+    category: "TV MOMS",
+    clue: "This character was mom to Marcia, Jan, Cindy, Greg, Peter & Bobby",
+    value: 100
+  },
+  "2019-12-06": {
+    category: "TV MOMS",
+    clue:
+      "Wendi McLendon-Covey plays mom Beverly on this sitcom set in 1980-something",
+    value: 100
+  },
+  "2019-12-07": {
+    category: "NATIONAL HISTORICAL PARKS",
+    clue:
+      "Established in 2015, the Manhattan Project National Historical Park has sites in Hanford, Washington; Los Alamos, N.M. & this Tennessee city",
+    value: 500
+  },
+  "2019-12-08": {
+    category: "CELEBRITY SUPERFANS",
+    clue:
+      "Stephen Colbert is such a fan of this author's work, he aired his concerns with Peter Jackson about the Elves of Mirkwood",
+    value: 100
+  },
+  "2019-12-09": {
+    category: "CELEBRITY SUPERFANS",
+    clue:
+      "Having grown up in Overland Park, Kansas, Paul Rudd is a rabid fan of this NFL team",
+    value: 100
+  },
+  "2019-12-10": {
+    category: "CELEBRITY SUPERFANS",
+    clue:
+      'NBA Hall of Famer Bill Walton followed this "Truckin" band to more than 850 shows',
+    value: 100
+  },
+  "2019-12-11": {
+    category: "21st CENTURY COMPANIES",
+    clue:
+      "The idea for this app was born in 2008 after its co-founders were unable to get a cab in France",
+    value: 100
+  },
+  "2019-12-12": {
+    category: "21st CENTURY COMPANIES",
+    clue:
+      "5 years after launching in 2012, this app was processing 1.4 billion swipes per day & had 10 billion matches",
+    value: 100
+  },
+  "2019-12-13": {
+    category: "21st CENTURY COMPANIES",
+    clue:
+      'A Swedish co-founder of this platform wished to "create a service that was better than piracy &...compensates the music industry"',
+    value: 100
+  },
+  "2019-12-14": {
+    category: "NICKELODEON SHOWS",
+    clue:
+      "This condiment was touted as an instrument by one starfish in Spongebob Squarepants",
+    value: 500
+  }
 } as const;
 
 export default clues;
--- src/utils/responses.ts
+++ src/utils/responses.ts
@@ -1,5 +1,18 @@
 const responses = {
-  "2019-12-11": "Mayonniase"
+  "2019-12-01": "Magna Carta",
+  "2019-12-02": "ATF",
+  "2019-12-03": "Citadel",
+  "2019-12-04": "That 70s Show",
+  "2019-12-05": "Carol Brady",
+  "2019-12-06": "The Goldbergs",
+  "2019-12-07": "Oak Ridges",
+  "2019-12-08": "Tolkien",
+  "2019-12-09": "Chiefs",
+  "2019-12-10": "Grateful Dead",
+  "2019-12-11": "Uber",
+  "2019-12-12": "Tinder",
+  "2019-12-13": "Spotify",
+  "2019-12-14": "Mayonnaise"
 } as const;
 
 export default responses;
