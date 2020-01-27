86c3374393a858b535ea9aa0971bfac6e72cf4ba
Amanda Bosson <amandabosson@yahoo.com>
2020-01-26 16:55:25 -0800

    Credit for arrow icons

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -3,6 +3,17 @@ import React from "react";
 const Account = ({ record, user }) => (
   <div>
     <div>Username: {user.displayName}</div>
+    <div style={{ position: "absolute", bottom: "16px" }}>
+      Arrow icons made by{" "}
+      <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+        Freepik
+      </a>{" "}
+      from{" "}
+      <a href="https://www.flaticon.com/" title="Flaticon">
+        {" "}
+        www.flaticon.com
+      </a>
+    </div>
   </div>
 );
 
